6e6ad3cb5bb85b67b6c11fb16fe8803b9fa5992d
Nadim <nadimfarah@live.com>
2013-10-09 12:10:43 +0300

    fall back for dynamic titles with tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,18 +9,28 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
 
 
-    it "should have the title 'Help'" do
+    it "should have the base title title " do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+    	visit'static_pages/help'
+    	expect(page).not_to_have_title('| Help')
     end
   end
 
@@ -31,9 +41,12 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
 
-    it "should have the title 'About Us'" do
+    it "should have the base title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App ")
     end
+    it "should not have a custom title" do
+    	visit'static_pages/about'
+    	expect(page).not_to_have_title('| About Us')
   end
 end
