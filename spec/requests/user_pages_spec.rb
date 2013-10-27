820098200bb6c5b7ef2a0bdc58196dcd98897273
Nadim <nadimfarah@live.com>
2013-10-27 13:54:58 +0300

    Cucumber

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,16 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  describe "with valid information" do
+  	describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+  end
 
   describe "signup page" do
     before { visit signup_path }
